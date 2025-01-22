a4d7e7b27c1c0741556ab98e22172877ab2463fe
“ramfox” <kasey@n0.computer>
2025-01-21 20:42:01 -0500

    fix: keep `endpoint.wait_idle` as the last task before closing
    This allows us to initiate shutdown for other parts of the magicsock, while the connections on the endpoint are closing. We `wait_idle` before we return from the magicsock `close` method, to ensure that all connections have been closed before we drop the endpoint.

--- iroh/src/magicsock.rs
+++ iroh/src/magicsock.rs
@@ -1780,11 +1780,17 @@ impl Handle {
     /// indefinitely after this call.
     #[instrument(skip_all, fields(me = %self.msock.me))]
     pub(crate) async fn close(&self) {
-        tracing::debug!("Closing connections");
+        tracing::warn!("Closing connections");
+
+        // Initiate closing all connections, and refuse future connections.
+        // We must call `self.endpoint.wait_idle` before exiting this method,
+        // to ensure all connections are closed gracefully before dropping
+        // the endpoint.
         self.endpoint.close(0u16.into(), b"");
-        self.endpoint.wait_idle().await;
 
         if self.msock.is_closed() {
+            // Wait for all connections to finish closing gracefully before exiting.
+            self.endpoint.wait_idle().await;
             return;
         }
         self.msock.closing.store(true, Ordering::Relaxed);
@@ -1815,6 +1821,9 @@ impl Handle {
             debug!("aborting remaining {}/3 tasks", tasks.len());
             tasks.shutdown().await;
         }
+
+        // Wait for all connections to finish closing gracefully before exiting.
+        self.endpoint.wait_idle().await;
     }
 }
 
